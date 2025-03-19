33cd7496fabb7ef99790df5ece2ac6b4b06e3eed
Frédéric Ferreira <frederic.ferreira66@gmail.com>
2025-03-19 09:40:41 +0100

    modified environ variables in settings.py file

--- project/settings.py
+++ project/settings.py
@@ -91,8 +91,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ['DB_PASSWORD_RAILWAY'],
         'HOST': os.environ.get('RAILWAY_HOST'),
         'PORT': os.environ.get('RAILWAY_PORT'),
